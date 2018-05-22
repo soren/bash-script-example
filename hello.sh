207481eaf4002c0e49d909dc8db55f00db3aa069
Søren Lund <soren@lund.org>
2018-05-22 11:30:49 +0200

    Add intial version

---
+++ hello.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Hello, World!"
+
+: <<'DOC'
+
+=head1 NAME
+
+Bash Script Example - an example of a Bash script
+
+=head1 SYNOPSIS
+
+    ./hello.sh
+
+=head1 DESCRIPTION
+
+This script is meant as an demonstration of writing a self-documenting
+script, where the documenation is also shown in GitHub (by softlinking
+README.pod to this script).
+
+=cut
+DOC
